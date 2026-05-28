3433196e9f7786b9c7b4693bd5b9dc51aa906903
Otto Uusihakala <otto.uusihakala@gmail.com>
2026-05-28 13:30:32 +0300

    OPHYK-529 fixed cas-oppija callerHenkiloOid test

--- src/test/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/RequestCallerFilterTest.java
+++ src/test/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/RequestCallerFilterTest.java
@@ -6,8 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.tomakehurst.wiremock.http.Cookie;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import java.io.IOException;
 import java.net.URI;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -23,7 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -36,23 +35,14 @@ public class RequestCallerFilterTest extends TiedotuspalveluApiTest implements R
 
   @Autowired private TestRestTemplate restTemplate;
 
-  @Autowired TiedotuspalveluProperties properties;
-
-  @LocalServerPort int port;
-
   @RegisterExtension
   static WireMockExtension wireMock =
-      WireMockExtension.newInstance()
-          .options(wireMockConfig().dynamicPort().extensions(CookieMirrorTransformer.class))
-          .build();
+      WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
   @DynamicPropertySource
   static void registerProperties(DynamicPropertyRegistry registry) {
     registry.add("tiedotuspalvelu.cas-oppija.server-url", wireMock::baseUrl);
     registry.add("tiedotuspalvelu.cas-virkailija.server-url", wireMock::baseUrl);
-    // registry.add("tiedotuspalvelu.cas-oppija.service-base-url", );
-    // registry.add("tiedotuspalvelu.suomifi-viestit.password", () -> SUOMIFI_PASSWORD);
-    // registry.add("tiedotuspalvelu.suomifi-viestit.sender-service-id", () -> SUOMIFI_SYSTEM_ID);
   }
 
   @BeforeEach
@@ -92,52 +82,112 @@ public class RequestCallerFilterTest extends TiedotuspalveluApiTest implements R
 
   @Test
   public void logsCallerHenkiloOidWhenCallerAuthenticatedWithCasOppija(CapturedOutput output)
-      throws Exception {
+      throws IOException, InterruptedException {
+    var cookie = getOppijaCookie();
+    var ticket = "ST-30-JVB-gESc2Yc3S-zV25JOHbVEeBo-ip-10-0-55-20";
+    createWiremockStubsForOppija(ticket, cookie);
 
+    var client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
+
+    var request =
+        HttpRequest.newBuilder()
+            .uri(
+                URI.create(
+                    wireMock.url(
+                        "/login?service="
+                            + URLEncoder.encode(
+                                "http://localhost:8080/omat-viestit/j_spring_cas_security_check",
+                                StandardCharsets.UTF_8))))
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build();
+
+    var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    var responseHeaders = response.headers();
+
+    var cookies = responseHeaders.allValues("Set-Cookie").get(0);
+
+    var tiedotteetRequest =
+        HttpRequest.newBuilder()
+            .header("Cookie", cookies)
+            .uri(URI.create("http://localhost:8080/omat-viestit/ui/tiedotteet"))
+            .GET()
+            .build();
+
+    client.send(tiedotteetRequest, HttpResponse.BodyHandlers.ofString());
+
+    assertThat(output).contains("\"callerHenkiloOid\": \"1.2.246.562.98.19783284870\"");
+  }
+
+  private Cookie getOppijaCookie() {
     var tgc = "TGC=asd";
     var cookiePath = "/cas-oppija";
-    var cookie =
-        new Cookie(
-            cookiePath,
-            tgc,
-            "SameSite=none",
-            "SameSite=None",
-            "Secure",
-            "HttpOnly",
-            "Path=/cas-oppija");
+    return new Cookie(
+        cookiePath,
+        tgc,
+        "SameSite=none",
+        "SameSite=None",
+        "Secure",
+        "HttpOnly",
+        "Path=/cas-oppija");
+  }
 
-    System.out.println("cookie " + cookie);
-    var ticket = "ST-30-JVB-gESc2Yc3S-zV25JOHbVEeBo-ip-10-0-55-20";
-    var serviceUrl =
-        URLEncoder.encode(
-            restTemplate.getRootUri() + "/omat-viestit/j_spring_cas_security_check",
-            StandardCharsets.UTF_8);
-    var location =
-        restTemplate.getRootUri() + "/omat-viestit/j_spring_cas_security_check?ticket=" + ticket;
-
+  private void createWiremockStubsForOppija(String ticketId, Cookie cookie) {
     wireMock.stubFor(
-        get(urlEqualTo("/login?service=" + serviceUrl))
+        get(urlEqualTo(
+                "/login?service="
+                    + URLEncoder.encode(
+                        "http://localhost:8080/omat-viestit", StandardCharsets.UTF_8)))
             .willReturn(
                 aResponse()
                     .withStatus(302)
                     .withHeader(
-                        "Location", URLDecoder.decode(serviceUrl, StandardCharsets.UTF_8))));
+                        "Location",
+                        "http://localhost:8080/omat-viestit/j_spring_cas_security_check?ticket="
+                            + ticketId)
+                    .withHeader("Set-Cookie", cookie.toString())));
+
     wireMock.stubFor(
-        post(urlEqualTo("/omat-viestit/cas-oppija/login?service=tiedotuspalvelu"))
+        post(urlEqualTo(
+                "/login?service="
+                    + URLEncoder.encode(
+                        "http://localhost:8080/omat-viestit/j_spring_cas_security_check",
+                        StandardCharsets.UTF_8)))
             .willReturn(
                 aResponse()
                     .withStatus(302)
-                    .withHeader("Location", location)
-                    .withHeader("Set-Cookie", cookie.getValue())));
+                    .withHeader(
+                        "Location",
+                        "http://localhost:8080/omat-viestit/j_spring_cas_security_check?ticket="
+                            + ticketId)
+                    .withHeader("Set-Cookie", cookie.toString())));
     wireMock.stubFor(
-        get(urlEqualTo("/p3/serviceValidate?ticket=%s&service=%s".formatted(ticket, serviceUrl)))
+        get(urlEqualTo(
+                "/login?service="
+                    + URLEncoder.encode(
+                        "http://localhost:8080/omat-viestit/j_spring_cas_security_check",
+                        StandardCharsets.UTF_8)))
+            .willReturn(
+                aResponse()
+                    .withStatus(302)
+                    .withHeader(
+                        "Location",
+                        "http://localhost:8080/omat-viestit/j_spring_cas_security_check?ticket="
+                            + ticketId)
+                    .withHeader("Set-Cookie", cookie.toString())));
+    // validate, provide cas response
+    wireMock.stubFor(
+        get(urlEqualTo(
+                "/p3/serviceValidate?ticket=%s&service=%s"
+                    .formatted(
+                        ticketId,
+                        URLEncoder.encode(
+                            "http://localhost:8080/omat-viestit/j_spring_cas_security_check",
+                            StandardCharsets.UTF_8))))
             .willReturn(
                 aResponse()
                     .withStatus(200)
                     .withBody(readResource("/cas-oppija-auth-response.xml"))));
-
-    // TODO: copy virkailija implementation for this test
-    assertThat(output).contains("\"callerHenkiloOid\": \"1.2.246.562.98.19783284870\"");
   }
 
   private Cookie getCookie() {
@@ -154,7 +204,6 @@ public class RequestCallerFilterTest extends TiedotuspalveluApiTest implements R
   }
 
   private void createWiremockStubs(Cookie cookie, String ticketId) {
-    // TODO: check which ones of these are actually necessary
     wireMock.stubFor(
         get(urlEqualTo(
                 "/login?service="
@@ -246,8 +295,7 @@ public class RequestCallerFilterTest extends TiedotuspalveluApiTest implements R
             .GET()
             .build();
 
-    var summaryResponse = client.send(summaryRequest, HttpResponse.BodyHandlers.ofString());
-    System.out.println("summaryResponse: " + summaryResponse);
+    client.send(summaryRequest, HttpResponse.BodyHandlers.ofString());
 
     assertThat(output).contains("\"callerHenkiloOid\": \"1.2.246.562.98.1234567890\"");
   }
